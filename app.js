c433f53ef5a3610afa4e60196a4ac861ff3b608a
Miguel Es <mig2190@hotmail.com>
2019-07-17 23:45:46 -0500

    setting up the first event listener

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 var budgetController = (function(){
-	var x = 23;
-
-	var add = function(a) {
-		return x + a;
-	}
-
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-	}
+	//
 })();
 
 var UIController = (function() {
@@ -21,12 +11,19 @@ var UIController = (function() {
 
 var controller = (function (budgetCtrl, UICtrl) {
 
-	var y = budgetCtrl.publicTest(5);
+	var ctrlAddItem = function() {
 
-	return {
-		anotherPublic: function() {
-			console.log(y);
-		}
+		console.log('this works');
 	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+
+	if (event.keyCode === 13 || event.which === 13) {
+
+		ctrlAddItem();
+	}
+	});
 
 })(budgetController, UIController);
