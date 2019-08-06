16c9153176b9580332c21bc177e65351635563f7
Miguel Escamilla <mig2190@hotmail.com>
2019-08-06 14:27:48 -0500

    added function to read input data on uicontroller

--- app.js
+++ app.js
@@ -3,20 +3,40 @@ var budgetController = (function(){
 })();
 
 var UIController = (function() {
+	//
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputbtn: '.add__btn'
+	}
+	return {
+		getInput: function() {
+			return {
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value
+			};
+		},
 
-	//
-	//
+		getDOMstrings: function() {
+			return DOMstrings;
+		}
+	};
 })();
 
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+	var DOM = UICtrl.getDOMstrings();
 	var ctrlAddItem = function() {
+		//Get the field input data
+		var input = UICtrl.getInput();
+		console.log(input);
 
-		console.log('this works');
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputbtn).addEventListener('click', ctrlAddItem);
 
 	document.addEventListener('keypress', function(event) {
 
