78b1d4559eb7b004339c48a35f800835c60f4d24
Miguel Escamilla <mig2190@hotmail.com>
2019-08-26 14:30:37 -0500

    added budget object on UI

--- app.js
+++ app.js
@@ -100,7 +100,11 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputbtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expenseContainer: '.expenses__list'
+		expenseContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	}
 	return {
 		getInput: function() {
@@ -149,6 +153,21 @@ var UIController = (function() {
 			fields[0].focus();
 			/*console.log(fieldsArr);*/
 			//console.log(fields);
+		},
+
+		displayBudget: function(obj) {
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+			if (obj.percentage > 0) {
+
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else {
+
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+			}
 		}
 	};
 })();
@@ -179,6 +198,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 		var budget = budgetCtrl.getBudget();
 		//3.Display the budget on the UI
 		console.log(budget);
+		UICtrl.displayBudget(budget);
 	};
 
 	var ctrlAddItem = function() {
@@ -203,6 +223,12 @@ var controller = (function (budgetCtrl, UICtrl) {
 		init: function() {
 			console.log('Application has started');
 			setupEventListeners();
+			UIController.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 		}
 	};
 
