16bbf559d44582e2bd320364b68cfc334bb534c3
Miguel Escamilla <mig2190@hotmail.com>
2020-03-11 13:29:10 -0600

    added formatting on numbers

--- app.js
+++ app.js
@@ -158,7 +158,25 @@ var UIController = (function() {
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
 		expensesPercLabel : '.item__percentage'
-	}
+	};
+
+	var formatNumber = function(num, type) {
+
+		var numSplit, int, dec, type;
+		num = Math.abs(num);
+		num = num.toFixed(2);
+
+		numSplit = num.split('.');
+		int = numSplit[0];
+
+		if (int.length > 3) {
+			int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+		}
+
+		dec = numSplit[1];
+
+		return (type === 'exp' ? sign = '-' : sign = '+') + ' ' + int + '.' + dec;
+	};
 	return {
 		getInput: function() {
 			return {
@@ -187,7 +205,7 @@ var UIController = (function() {
 			//replace the placeholder text with some actual data
 			newHtml = html.replace('%id%', obj.id);
 			newHtml = newHtml.replace('%description%', obj.description);
-			newHtml = newHtml.replace('%value%', obj.value);
+			newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 			//Insert HTML into the DOM
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 		},
@@ -214,9 +232,12 @@ var UIController = (function() {
 
 		displayBudget: function(obj) {
 
-			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			var type;
+			obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+			document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+			document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
 
 			if (obj.percentage > 0) {
 
