6ce6a14f0891ddc1151ec795504649300aba73e3
Miguel Escamilla <mig2190@hotmail.com>
2020-03-11 12:29:05 -0600

    updated percentages on UI

--- app.js
+++ app.js
@@ -156,7 +156,8 @@ var UIController = (function() {
 		incomeLabel: '.budget__income--value',
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
-		container: '.container'
+		container: '.container',
+		expensesPercLabel : '.item__percentage'
 	}
 	return {
 		getInput: function() {
@@ -224,6 +225,29 @@ var UIController = (function() {
 
 				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
 			}
+		},
+
+		displayPercentages: function(percentages) {
+
+			console.log(percentages);
+
+			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+			var nodeListForEach = function(list, callback) {
+
+				for (var i = 0; i < list.length; i++) {
+					callback(list[i], i);
+				}
+			};
+			nodeListForEach(fields, function(current, index){
+
+				if (percentages[index] > 0) {
+
+					current.textContent = percentages[index] + '%';
+				}else {
+					current.textContent = '---';
+				}
+			});
 		}
 	};
 })();
@@ -266,7 +290,8 @@ var controller = (function (budgetCtrl, UICtrl) {
 		//2. Read percentahes from the budget controller
 		var percentages = budgetCtrl.getPercentages();
 		//3-. Update the UI
-		console.log(percentages);
+		//console.log(percentages);
+		UICtrl.displayPercentages(percentages);
 	};
 	var ctrlAddItem = function() {
 		//Get the field input data
